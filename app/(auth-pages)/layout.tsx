88a737abaa38d810cedbbae04d81005b30e8e6ac
lequoctrung-it <leqtrungit@gmail.com>
2025-05-01 10:19:24 +0700

    Fixed layout for auth page

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -4,8 +4,10 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
-      {children}
+    <div className="min-h-screen w-full flex items-center justify-center overflow-hidden px-4 fixed inset-0">
+      <div className="w-full max-w-md">
+        {children}
+      </div>
     </div>
   );
 }
